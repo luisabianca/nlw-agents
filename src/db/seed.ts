3818c173b1af4ff25c2ae029c85f36fa5e837f53
Bianca Santos <luisabianca93@gmail.com>
2025-07-08 14:20:46 -0300

    finish second day

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,21 +1,22 @@
-import { reset, seed } from "drizzle-seed";
-import { schema } from "./schema/index.ts";
-import { db, sql } from "./connection.ts";
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
-await reset(db, schema);
-await seed(db, schema).refine(f => {
+await reset(db, schema)
+
+await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
-
-      }
-    }
+      },
+    },
+    questions: {
+      count: 20,
+    },
   }
 })
 
-await sql.end();
-
-console.log('Database seeded successfully');
+await sql.end()
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,37 @@
-import fastify from "fastify";
-import { sql } from "./db/connection.ts";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
-import {fastifyCors} from "@fastify/cors";
-import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { fastifyCors } from '@fastify/cors'
+// import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+// import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+// import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-origin: '*'
-});
+  origin: 'http://localhost:5173',
+})
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+// app.register(fastifyMultipart)
 
-app.get("/health", () => {
-  return "ok";
-});
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-const start = async () => {
-  try {
-    await app.listen({ port: env.PORT });
-    console.log(`🚀 HTTP server running on http://localhost:${env.PORT}`);
-  } catch (err) {
-    console.error("❌ Erro ao iniciar o servidor", err);
-    process.exit(1);
-  }
-};
+app.get('/health', () => {
+  return 'OK'
+})
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+// app.register(createQuestionRoute)
+// app.register(uploadAudioRoute)
 
-start();
+app.listen({ port: env.PORT })
